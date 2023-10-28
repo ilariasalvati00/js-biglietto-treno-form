a072d7e8855f216d3ba827e8070af6b3d6e73578
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-10-29 00:14:44 +0200

    anti-stupido

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const element = document.getElementById("invia");
 element.addEventListener('click',
 function(){
-    const km = document.getElementById("chilometri").value;
-    let prezzo_biglietto = 0.21*km
-    const eta = document.getElementById("eta").value
-    if(eta<18){
-        prezzo_biglietto= prezzo_biglietto*0.8;
-    } else if(eta>65){
-        prezzo_biglietto = prezzo_biglietto*0.6;
+    const km = Number(document.getElementById("chilometri").value);
+    if (! isNaN(km)){
+        let prezzo_biglietto = 0.21*km
+        const eta = Number(document.getElementById("eta").value);
+        if (! isNaN(eta)){
+            if(eta<18){
+                prezzo_biglietto= prezzo_biglietto*0.8;
+            } else if(eta>65){
+                prezzo_biglietto = prezzo_biglietto*0.6;
+            }
+            prezzo_biglietto = prezzo_biglietto.toFixed(2);
+            console.log(prezzo_biglietto)
+        }
     }
-    prezzo_biglietto = prezzo_biglietto.toFixed(2);
-    console.log(prezzo_biglietto)
 });
 
 
