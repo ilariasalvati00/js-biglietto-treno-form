47c94c45cedaa3277331f9b0556f539dd79ea638
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-10-29 02:35:36 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -3,18 +3,45 @@ element.addEventListener('click',
 function(){
     const km = Number(document.getElementById("chilometri").value);
     if (! isNaN(km)){
-        let prezzo_biglietto = 0.21*km
-        const eta = Number(document.getElementById("eta").value);
-        if (! isNaN(eta)){
-            if(eta<18){
-                prezzo_biglietto= prezzo_biglietto*0.8;
-            } else if(eta>65){
-                prezzo_biglietto = prezzo_biglietto*0.6;
-            }
-            prezzo_biglietto = prezzo_biglietto.toFixed(2);
-            console.log(prezzo_biglietto)
+        let prezzo_biglietto = 0.21*km;
+        const eta = document.getElementById("fascia-eta").value;
+        let offerta = "Biglietto Standard";
+        if(eta == "minorenne"){
+            prezzo_biglietto= prezzo_biglietto*0.8;
+            offerta = "Biglietto Ridotto Minorenne";
+        } else if(eta == "over65"){
+            prezzo_biglietto = prezzo_biglietto*0.6;
+            offerta = "Biglietto Ridotto Over 65";
         }
+        prezzo_biglietto = prezzo_biglietto.toFixed(2);
+
+        let nome = document.getElementById("nome").value;
+        document.getElementById("destinazione_nome").innerHTML = nome;
+
+        document.getElementById("offerta").innerHTML = offerta;
+
+        document.getElementById("prezzo").innerHTML = `${prezzo_biglietto}€`;
+
+        document.getElementById("header-biglietto").classList.remove("no-display");
+        document.getElementById("biglietto").classList.remove("no-display");
+
+
     }
 });
 
+const element2 = document.getElementById("annulla")
+element2.addEventListener('click', 
+function(){
+    document.getElementById("chilometri").value = "";
+    document.getElementById("fascia-eta").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("destinazione_nome").innerHTML = "";
+
+    document.getElementById("offerta").innerHTML = "";
+
+    document.getElementById("prezzo").innerHTML = "";
+
+    document.getElementById("header-biglietto").classList.add("no-display");
+    document.getElementById("biglietto").classList.add("no-display");
+})
 
